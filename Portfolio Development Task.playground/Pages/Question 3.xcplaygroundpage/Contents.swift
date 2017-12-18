9ca918677f6b87b98131ef03eaf08509a74d91b7
Russell Gordon <rgordon@lcs-mba2410.local>
2017-12-18 11:07:24 -0500

    Updated wording for question 3.

--- Portfolio Development Task.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 3.xcplaygroundpage/Contents.swift
@@ -1,22 +1,22 @@
 /*:
  # Question 3
  
- In the old days, people used a machine to count the value of spare change:
+ You are working to develop your leadership skills at Lakefield and come up with a series of challenges that you will encourage students to complete.
  
- ![coin_sorter](coin_sorter.jpg "A coin sorter")
+ A generous group of LCS alumni have voluteered to donate money to the Canadian Cancer Society charity based on how many students complete each challenge.
  
- Forget a dedicated coin-sorting machine - imagine that you are writing a cutting edge augmented-reality app that will count coins just by pointing the camara of your phone at a pile of coins spread out on a table.
+ ![ccs](logo_ccs_society.png "Canadian Cancer Society")
  
- Create a variable called `valueOfDimes`. Set it to `0.10` .
+ For every student that volunteers to shave their head to help with the creation of wigs, the donors will contribute $55.75. Create a constant called `hairDonationAmount` with an appropriate data type and initialize it according to the amount of the donation.
  
- Create a variable called `dimeCount`. It will track the number of dimes. Select the most appropriate data type. You can choose how many dimes your app counted.
+ For every student that completes a Terry Fox Run training session of at least 60 minutes, the donors will contribute $15.35. Create a constant called `runTrainingDonationAmount` with an appropriate data type and initialize it according to the amount of the donation.
+ 
+ So far 12 students have shaved their head to donate hair. Create a variable called `headShavedCount` with an appropriate data type to track the count of students who have so far chosen to shave their head.
 
- Create a variable called `valueOfQuarters`. Set it to `0.25` .
+ So far 73 students have completed a 60-minute Terry Fox Run training session. Create a variable called `runTrainingCount` with an appropriate data type to track the count of students who have completed a training session.
+ 
+ Now, create a variable called `totalDonationAmount` and initialize using a single expression, with appropriate operators, to calculate the current total dollar amount that will be donated to the Canadian Cancer Society.
 
- Create a variable called `quarterCount`. It will track the number of quarters. Select an appropriate data type. You can choose how many quarters your app counted.
- 
- Now create a constant called `totalValueOfCoins` and initialize the constant using a single expression that accurately calculates the total value of all the coins.
- 
  Finally, for each variable and constant, explain why you used a given data type. Use `print()` statements to do this.
  
  */
