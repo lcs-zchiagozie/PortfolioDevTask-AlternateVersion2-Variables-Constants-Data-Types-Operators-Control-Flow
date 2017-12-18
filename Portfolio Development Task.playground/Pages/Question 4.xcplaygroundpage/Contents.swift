5a49cb6f99acd05656b41456f7367e8622e453d7
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2017-12-18 11:11:03 -0500

    Modifications to numbers.

--- Portfolio Development Task.playground/Pages/Question 4.xcplaygroundpage/Contents.swift
+++ Portfolio Development Task.playground/Pages/Question 4.xcplaygroundpage/Contents.swift
@@ -9,13 +9,15 @@
  
  For each point below, use the correct *operator* to update the balance of the money you have earned by completing the dares.
  
- 1. Your friend dares you to tell a bad joke in a really enthusiastic way to your housemates. You accept and perform the task brilliantly, earning $5. Use a *compound assignment operator* to update `moneyIHaveEarned`.
- 2. You dare your friend to improvise a rap song about the next person that enters the dorm. She accepts and does an amazing job. You owe her $2 for this dare. Update `moneyIHaveEarned` using a *compound assignment operator*.
- 3. Your friend dares you to stand up in the Dining Hall on the last day of classes before Christmas Break and sing "I'm a Little Teapot" very loudly with all the appropriate actions. You agree only if she will quadruple your current earnings. Terms are accepted and you perform the song in the dining hall as required. Update `moneyIHaveEarned` using a *compound assignment operator*.
- 4. You dare your friend to stand safely by the side of County Road 29 and smile and wave enthusiastically at every vehicle that goes by. For each minute that she does this you will pay her, from your earnings, 75 cents. She does this for 13 minutes. Update `moneyIHaveEarned` using a compound assignment operator and an appropriate arithmetic operator.
+ 1. Your friend dares you to tell a bad joke in a really enthusiastic way to your housemates. You accept and perform the task brilliantly, earning $12. Use a *compound assignment operator* to update `moneyIHaveEarned`.
+ 2. You dare your friend to improvise a rap song about the next person that enters the dorm. She accepts and does an amazing job. You owe her $7 for this dare. Update `moneyIHaveEarned` using a *compound assignment operator*.
+ 3. Your friend dares you to stand up in the Dining Hall on the last day of classes before Christmas Break and sing "I'm a Little Teapot" very loudly with all the appropriate actions. You agree only if she will triple your current earnings. Terms are accepted and you perform the song in the dining hall as required. Update `moneyIHaveEarned` using a *compound assignment operator*.
+ 
  
  ![teapot](teapot.png "I'm a little teapot")
 
+ 4. You dare your friend to stand safely by the side of County Road 29 and smile and wave enthusiastically at every vehicle that goes by. For each minute that she does this you will pay her, from your earnings, 50 cents. She does this for 11 minutes. Update `moneyIHaveEarned` using a compound assignment operator and an appropriate arithmetic operator.
+ 
  
  */
 // Answer question 4 below
